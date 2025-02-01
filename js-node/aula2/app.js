ed2eab13d5c57cc7cf84fb4807bb05bc88d8d8bd
Erik-Arch-Notebook <erikpdn1@gmail.com>
2025-02-01 12:37:22 -0300

    aprendendo a sobre como navegar entre os modulos

---
+++ js-node/aula2/app.js
@@ -0,0 +1,7 @@
+// const DogClass = require('./b/c/mod');
+//
+// const dog1 = new DogClass('Toby');
+// dog1.bark(); 
+
+const path = require('path');
+console.log(path.resolve(__dirname, '..', '..', 'js-modules'));
