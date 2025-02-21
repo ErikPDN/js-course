c9ce87641a5b4735452f0f60e1a3ac0efebbb54e
Erik-Arch-Notebook <erikpdn1@gmail.com>
2025-02-21 15:01:21 -0300

    criando o middleware de autenticação usando o jwt

--- api-rest/src/routes/userRoutes.js
+++ api-rest/src/routes/userRoutes.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import userController from '../controllers/UserController';
+import authMiddleware from '../middlewares/loginRequired';
 
 const router = new Router();
 
-router.post('/', userController.create);
-router.get('/', userController.index);
-router.get('/:id', userController.show);
-router.delete('/:id', userController.delete);
-router.put('/:id', userController.update);
+router.post('/', authMiddleware, userController.create);
+router.get('/', authMiddleware, userController.index);
+router.get('/:id', authMiddleware, userController.show);
+router.delete('/:id', authMiddleware, userController.delete);
+router.put('/:id', authMiddleware, userController.update);
 
 export default router;
