03f4ebb87e05c80a272f2e571d680aba400858a5
Erik-Arch-Notebook <erikpdn1@gmail.com>
2025-03-07 13:25:38 -0300

    substituindo o browserRoute pelo history e criando o MyRoute

--- react-base/src/routes/index.jsx
+++ react-base/src/routes/index.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
+import MyRoute from './MyRoute';
 import Login from '../pages/login';
 import Page404 from '../pages/page404';
 
 export default function Routes() {
   return (
     <Switch>
-      <Route path="/" exact component={Login} />
-      <Route path="*" component={Page404} />
+      <MyRoute path="/" exact component={Login} />
+      <MyRoute path="*" component={Page404} />
     </Switch>
   )
 }
