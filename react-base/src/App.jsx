b62fe0c86ac75bba5e064069cfa3ceaf045797bb
Erik-Arch-Notebook <erikpdn1@gmail.com>
2025-03-08 11:59:15 -0300

    adicionando e aprendendo sobre redux em react

--- react-base/src/App.jsx
+++ react-base/src/App.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import { Provider } from 'react-redux';
 
 import history from './services/history';
 import Header from './components/header';
 import GlobalStyle from './styles/global';
 import Routes from './routes';
+import store from './store';
 
 function App() {
   return (
-    <Router history={history}>
-      <Header />
-      <Routes />
-      <GlobalStyle />
-      <ToastContainer autoClose={3000} className="toast-container" />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <Header />
+        <Routes />
+        <GlobalStyle />
+        <ToastContainer autoClose={3000} className="toast-container" />
+      </Router>
+    </Provider>
   )
 }
 
