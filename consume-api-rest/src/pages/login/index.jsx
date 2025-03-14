da8519ecb6b59d3fe96f4e770efd2774a6ec911b
Erik-Arch-Notebook <erikpdn1@gmail.com>
2025-03-14 00:41:42 -0300

    iniciando a configuraçao do reducer e saga na pagina de login da palicação

--- consume-api-rest/src/pages/login/index.jsx
+++ consume-api-rest/src/pages/login/index.jsx
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
+import { isEmail } from 'validator';
+import { useDispatch } from 'react-redux';
 
 import { Container } from '../../styles/global';
+import { Form } from './styled.js';
+import * as actions from '../../store/modules/auth/actions';
 
 export default function Login() {
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let formErrors = false;
+
+    if (!isEmail(email)) {
+      formErrors = true;
+      toast.error('Invalid email');
+    }
+
+    if (password.length < 6 || password.length > 50) {
+      formErrors = true;
+      toast.error('Invalid password');
+    }
+
+    if (!formErrors) {
+      dispatch(actions.loginRequest({ email, password }));
+    }
+
+  }
 
   return (
     <Container>
       <h1>Login</h1>
-    </Container>
+      <Form onSubmit={handleSubmit}>
+        <input type="email" name="" placeholder='Email' value={email} onChange={e => setEmail(e.target.value)} />
+        <input type="password" name="" placeholder='Password' value={password} onChange={e => setPassword(e.target.value)} />
+        <button type="submit">Login</button>
+      </Form>
+    </Container >
   )
 } 
--- consume-api-rest/src/pages/login/styled.js
+++ consume-api-rest/src/pages/login/styled.js
@@ -1,11 +1,29 @@
 import { styled } from "styled-components";
+import * as colors from '../../config/color.js';
 
-export const Title = styled.h1`
-  small {
-    font-size: 12pt;
-    margin-left: 10px;
+export const Form = styled.form`
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  
+  input { 
+    margin-bottom: 20px;
+    height: 40px;
+    padding: 0 10px;
+    border-radius: 4px;
+    border: 1px solid #cccccc;
+  
+    &:focus {
+      border: 2px solid ${colors.primaryColor};
+      outline: none;
+    } 
+  }
+  
+  button {
+    width: 100%;
+    padding: 16px 0;
+    font-size: 16px;
+    outline: none;
   }
 `;
-
-export const Paragraph = styled.p``;
 
