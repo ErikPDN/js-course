fbcb903a01c2a52b9cf0efb62bb6b7e9447953a9
Erik-Arch-Notebook <erikpdn1@gmail.com>
2025-03-19 13:18:12 -0300

    editando a foto do aluno e terminando a aplicação

--- consume-api-rest/src/pages/photos/styled.js
+++ consume-api-rest/src/pages/photos/styled.js
@@ -1,11 +1,35 @@
 import { styled } from "styled-components";
 
 export const Title = styled.h1`
-  small {
-    font-size: 12pt;
-    margin-left: 10px;
-  }
+  text-align: center;
+  
 `;
 
-export const Paragraph = styled.p``;
+export const Form = styled.form`
+  label {
+    width: 180px;
+    height: 180px;
+    display: flex;
+    background: #ddd;
+    border: 5px dashed;
+    margin: 30px auto;
+    border-radius: 50%;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    overflow: hidden;
+  }
+
+  input {
+    display: none;
+  }
+
+  img {
+    height: 180px;
+    width: 180px;
+    border-radius: 50%;
+  }
+  
+`
+
 
