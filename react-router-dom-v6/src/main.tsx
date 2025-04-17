1d05c69ba521ffceb84bec2f736c510750ceab6e
Erik-Arch-Notebook <erikpdn1@gmail.com>
2025-04-17 15:41:48 -0300

    aprendendo sobre diferença de Link e 'a'

--- react-router-dom-v6/src/main.tsx
+++ react-router-dom-v6/src/main.tsx
@@ -1,18 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import './styles/global.css'
 import { Home } from './components/Home'
 import { About } from './components/About'
+import { Menu } from './components/Menu'
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+      <Menu />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 )
