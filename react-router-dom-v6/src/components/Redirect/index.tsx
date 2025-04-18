9e350dfe7f94f7e5b7f9bca3246224d76dea93be
Erik-Arch-Notebook <erikpdn1@gmail.com>
2025-04-17 23:35:23 -0300

    redirecionamento com useNavigate

---
+++ react-router-dom-v6/src/components/Redirect/index.tsx
@@ -0,0 +1,29 @@
+import './style.css';
+import { useState, useRef, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export const Redirect = () => {
+  const [time, setTime] = useState(5);
+  const timeout = useRef(0);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    clearTimeout(timeout.current);
+
+    timeout.current = window.setTimeout(() => {
+      setTime((time) => time - 1);
+    }, 1000);
+
+    if (time <= 0) {
+      navigate('/');
+    }
+
+
+  }, [time, navigate]);
+
+  return (
+    <div>
+      <h1>Get out of here in: {time}</h1>
+    </div>
+  )
+}
